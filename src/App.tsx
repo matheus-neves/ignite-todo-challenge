a925bae8c0834f8fa615d16efdff47ee4cfcf71b
Matheus Neves <matneves@SAO-C02G620KML7H.local>
2022-06-13 12:00:07 -0300

    chore: add styled-components and define globalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { GlobalStyles } from '@styles/globalStyles';
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello World</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+
+  :root {
+    --gray-100: #f2f2f2;
+    --gray-200: #d9d9d9;
+    --gray-300: #808080;
+    --gray-400: #333333;
+    --gray-500: #262626;
+    --gray-700: #0d0d0d;
+
+    --blue-300: #4ea8de;
+    --blue-500: #1e6f9f;
+
+    --purple-300: #8284fa;
+    --purple-500: #5e60ce;
+
+    --red: #e25858;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: var(--gray-400);
+    color: var(--gray-100);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: Inter, sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`;
